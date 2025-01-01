950f5abb851bf094e2e1e446e941a1478047e29b
Saadi-cloud <muhammadsaadm95@gmail.com>
2025-01-01 20:55:47 +0500

    working on  modals and querysets

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, ProductImage
+from .models import Product, ProductImage, ChaiVarity, Store, ChaiReview, ChaiCertificate
 # Register your models here.
 
 
@@ -18,4 +18,20 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['price']
     list_per_page = 10
 
+# Register your models here.
+class ChaiReviewInline (admin.TabularInline):
+  model = ChaiReview
+  extra = 2
+class ChaiVarietyAdmin (admin.ModelAdmin):
+  list_display = ('name', 'type', 'date_added')
+  inlines = [ChaiReviewInline]
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varieties',)
 
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')
+
+admin.site.register(ChaiVarity, ChaiVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
